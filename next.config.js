9b5f1fda1dfcc8eb6ee6d7f2805a03c2633ed860
lliieeoo <1792087128@qq.com>
2026-05-21 16:27:44 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  api: {
-    bodyParser: {
-      sizeLimit: '50mb',
-    },
-    responseLimit: false,
-  },
 };
 
 module.exports = nextConfig;
